e2fb0962121a3e85be01be98e511ae4a9c840631
Quinn Lapish <quinn.lapish99@gmail.com>
2019-07-10 15:13:39 -0400

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,28 +3,18 @@
 
 var wins= 0;
 var failures= 0;
-var randomgem;
 var objectivenumber ="";
-var gem1;
-var gem2;
-var gem3;
-var gem4;
+var usernumber= 0;
+var gem1 =0;
+var gem2 =0;
+var gem3 =0;
+var gem4 =0;
 
 
 //start game function//
 
 function startgame ()
 {
-
-    // random gem goal// 
-
-
-objectivenumber = [Math.floor (Math.random () * (120 - 19)+ 19)];
-console.log(objectivenumber)
-}
-
-startgame();
-
 // four random gems // 
 
 gem1 = [Math.floor (Math.random () * 12 + 1)];
@@ -39,42 +29,38 @@ console.log(gem3)
 gem4 = [Math.floor (Math.random () * 12 + 1)];
 console.log(gem4)
 
+    // random gem goal// 
 
-
-
-
-document.onkeypress = function (event) {
-
-var usermove = prompt ("Pick a letter");
-
-if (usermove == computermove) 
-{wins++;
-console.log(usermove);
-
-document.querySelector("#wins").innerHTML = wins;
-
-console.log(wins);  
-startgame();  
-}
-else {
-
-document.querySelector("#usermove").append(usermove + ", ");
-
-guesses--;
-document.querySelector("#guesses").innerHTML = guesses;
-console.log(guesses)
+usernumber = 0;
+objectivenumber = [Math.floor (Math.random () * (120 - 19)+ 19)];
+console.log(objectivenumber);
+$("#computertotal").text(objectivenumber);
 }
 
-if (guesses == 0) 
+// rules //
+startgame();
 
-{failures++
+$("#usertotal").text(usernumber);
 
-document.querySelector("#failures").innerHTML = failures;
+if (usernumber == objectivenumber)
+{
+wins++;
+console.log(usernumber)
+$("#wins").text(wins);
+}
+
+else if (usernumber > objectivenumber)
+
+{
+
+    failures ++; 
+    $("#failures").text(failures);
+}
+
 
 startgame();
 
 console.log(failures)
-}
-}
+
 
 
